36784aa51f1a07003759980b14c172db4ad5f22d
derrick868 <derrickndirangu868@gmail.com>
2025-03-15 05:28:58 +0300

    Add files via upload

---
+++ website/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+from flask_migrate import Migrate
+import os
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')  
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    
+    db.init_app(app)
+    migrate = Migrate(app, db)  # Enable migrations
+
+
+
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note  # Ensure models are imported after db.init_app(app)
+
+    create_database(app)  # Call the function correctly
+
+   
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))  # Ensure `User` is imported
+
+    return app
+
+def create_database(app):
+    with app.app_context(): 
+        db.create_all()  
+        print('Ensured Database Tables Exist!')
